88ce14c019d3584b6981685c12b3821f2b4ade9f
BELANGER <julien.belanger@orange.fr>
2019-02-12 15:59:27 +0100

    fonctions 3 fichiers Flight

--- Controller/ControllerFlight.php
+++ Controller/ControllerFlight.php
@@ -8,4 +8,18 @@ class ControllerHome extends ControllerBase
     public function __construct() {
         parent::__construct("Flight");
     }
+
+/* Création d'un vol*/
+public function newFlight() {
+    $user=GET_['id_user']
+    $this->model->newFlight2($user);
+    }
+
+/* Envoi sur session*/
+public function addSession() {
+    $list=$this->model->addSession2($list);
+}
+
+
+
 }
--- View/ViewFlight.php
+++ View/ViewFlight.php
@@ -5,4 +5,18 @@ class ViewFlight extends ViewBase
     public function __construct() {
         parent::__construct();
     }
+
+public function getUser3($list){
+    $this->myHTML .="Bonjour ".$element[0]."voici le score de votre vol;
+}
+
+
+
+
+
+
+
+
+
+
 }
