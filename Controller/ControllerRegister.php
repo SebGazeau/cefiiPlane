311a82f21203871011c25fbe629e9b4be4ed41d9
DIMANCHIN Clément <clementdimanchin@free.fr>
2019-02-12 11:20:35 +0100

    test

--- Controller/ControllerRegister.php
+++ Controller/ControllerRegister.php
@@ -8,4 +8,8 @@ class ControllerRegister extends ControllerBase
     public function __construct() {
         parent::__construct("Register");
     }
+    
+    
+    
+    vfjdbvjfdnjvndfkjvbjkdbvjbdkjbvkbdk
 }
