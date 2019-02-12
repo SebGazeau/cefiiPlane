5c5492f90526de46d3395eeb96b049da2fc9aa1a
DIMANCHIN Clément <clementdimanchin@free.fr>
2019-02-12 14:37:47 +0100

    correction 1

--- Model/ModelRegister.php
+++ Model/ModelRegister.php
@@ -3,7 +3,7 @@
 class ModelRegister extends ModelBase
 {
 public function verifEmail($email){
-        $requete = $this->connexion->prepare("SELECT email FROM user WHERE email = :email");
+        $requete = $this-> databasse ->prepare("SELECT email FROM user WHERE email = :email");
 		$requete->bindParam(':email',$email);
 		$result = $requete -> execute();
         $email = $requete -> fetchALL(PDO::FETCH_NUM);
@@ -15,15 +15,15 @@ public function verifEmail($email){
 		return $exist;
     }  
 public function addUser($info){
-    $requete = $this->connexion->prepare("INSERT INTO user (id, nom, email,password) VALUES (NULL,:nom,:email,:password)");
-$requete->bindParam(':nom',$info['nom']);;
+    $requete = $this->databasse->prepare("INSERT INTO user (id, name, email,password) VALUES (NULL,:name,:email,:password)");
+$requete->bindParam(':name',$info['name']);;
 $requete->bindParam(':email',$info['email']);
 $requete->bindParam(':password',$info['password']);
 $result = $requete -> execute();
   }
-public function getIdUser($id){
-        $requete = $this -> connection -> prepare('SELECT id FROM user WHERE email = :email');
-        $requete->bindParam(':email',$info['email']);
+public function getIdUser($email){
+        $requete = $this -> databasse -> prepare('SELECT id FROM user WHERE email = :email');
+        $requete->bindParam(':email',$email);
         $id = $requete -> fetchALL(PDO::FETCH_NUM);
         $id=$id[0][0];
         return $id;
