823065798e0892d58377ccbaccd5d884d69fca31
Fred <hemery.f@gmail.com>
2019-02-12 14:50:56 +0100

    mise a jour

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function ($) {
     jQuery("#testPopup").click(popScore);
-})
+});
 
-function popScore() {
+function popScore(){
     // ouverture-fermeture popup
     jQuery("#contenuPopupFin").slideToggle();
+    jQuery("body").not($(this)).css("opacity","0.2");
+    jQuery("body").not($(this)).css("opacity","1");
 }
