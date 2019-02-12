cb49cec191a193f98748eb57ef6ee030ea11c2a2
SebGazeau <sebastiengazeau4@gmail.com>
2019-02-12 15:04:56 +0100

    correction js v2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 
 $(document).ready(function ($) {
     jQuery("#testPopup").click(popScore);
-<<<<<<< HEAD
     jQuery("#submit").click(popForm);
-=======
->>>>>>> c34e57b9567cf8d55fe1b13426f26023c979916f
 });
 
 function popScore(){
     // ouverture-fermeture popup
     jQuery("#contenuPopupFin").slideToggle();
-<<<<<<< HEAD
 }
 
 
@@ -18,9 +14,7 @@ function popScore(){
 function popForm() {
     // ouverture-fermeture popup
     jQuery("#popupForm").slideToggle();
-}
-=======
     jQuery("body").not($(this)).css("opacity","0.2");
     jQuery("body").not($(this)).css("opacity","1");
 }
->>>>>>> c34e57b9567cf8d55fe1b13426f26023c979916f
+
