b09e320ba3335aa09d90e37c7ca56e1bce462ce4
Loïc <loic.libertpisani@gmail.com>
2019-02-13 09:06:18 +0100

    ajout fonction FRED

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-
 $(document).ready(function ($) {
-   jQuery("#testPopup").click(popScore);
+    jQuery("#testPopup").click(popScore);
 });
 
-
-
-
 function popScore() {
-   jQuery("#contenuPopupFin").slideToggle();
+    // ouverture-fermeture popup
+    var transp = $("#popupForm,img").css("opacity");
+    var pop = $("#contenuPopupFin").attr("pop");
+    var slide = $("#popupFin").attr('class');
+    //    console.log (slide);
+
+    if (transp == "1") {
+        $("#popupForm,img").css('opacity', '0.2');
+    } else {
+        $("#popupForm,img").css('opacity', '1');
+    }
+    if (pop == "pop") {
+        $("#contenuPopupFin").removeClass("pop");
+    } else {
+        $("#contenuPopupFin").addClass("pop");
+    }
+    if (slide == "arcade slide") {
+        $("#popupFin").removeClass("slide");
+    } else {
+        $("#popupFin").addClass("slide");
+    }
+
+    jQuery("#contenuPopupFin").fadeToggle(100);
 }
